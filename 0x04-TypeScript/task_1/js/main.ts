b38c38413ccc65130ee2cb0a52e5207141f8d3e9
Ayman Elfadl <aelfadl@c3r9p10.1337.ma>
2025-05-23 14:25:34 +0100

    ok

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,10 +15,33 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+ 
+    constructor (public firstName:string, public lastName:string){}
+ 
+    workOnHomework(): string {
+        return "Curently working ..."
+    }
+ 
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
 const printTeacher: printTeacherFunction = function(firstName, lastName) {
   return firstName.charAt(0) + ". " + lastName;
 };
-
 
 const teacher1: Teacher = {
   firstName: "Ayman",
@@ -37,5 +60,15 @@ const director1: Directors = {
 };
 
 console.log(teacher1);
+
 console.log(director1);
+
 console.log(printTeacher("Ayman", "Elfadl"));
+
+const student = new StudentClass("Ayman", "Elfadl");
+
+console.log(student);
+
+console.log(student.displayName());
+console.log(student.workOnHomework());
+
